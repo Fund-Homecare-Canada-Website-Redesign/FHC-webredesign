4caad95b852bd82d82abc74067d5cd5c901076f3
Noah Christopher Kostesku <nkostes@uwo.ca>
2025-02-17 17:07:14 -0500

    Added most of the assets needed, still need some more from clients

---
+++ fhc-webred/src/assets/styles/BrandColours.js
@@ -0,0 +1,10 @@
+// ONLY USE THESE COLOURS, pass in as a paramter to customize components
+const colours = {
+    RAISIN_BLACK: "#231F20",
+    WHITE: "#FFFFFF",
+    BLUE_MUNSELL: "#3A92A0",
+    REBECCA_PURPLE: "#673490",
+    BLUE_YONDER: "#54749D",
+  };
+  
+  export default colours;
