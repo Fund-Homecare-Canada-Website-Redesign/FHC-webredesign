ad72cd8190b170f8f7e432a0bed0491779673b64
Noah Kostesku <144395446+noahkostesku@users.noreply.github.com>
2025-02-23 21:32:06 -0500

    Update App.js

--- fhc-webred/src/App.js
+++ fhc-webred/src/App.js
@@ -2,17 +2,27 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//delete in production
+import TestPage from "./TestPage";
+
+//ignore default code below
+
+// function App() {
+//   return (
+//     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+//       <header className="text-center bg-red-500 text-white p-6 rounded-lg shadow-lg">
+//         <p className="mt-4 text-lg font-bold text-gray-200">
+//           Tailwind is working if this text is styled!
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function App() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <header className="text-center bg-red-500 text-white p-6 rounded-lg shadow-lg">
-        <p className="mt-4 text-lg font-bold text-gray-200">
-          Tailwind is working if this text is styled!
-        </p>
-      </header>
-    </div>
-  );
+  return <TestPage />;
 }
 
 export default App;
