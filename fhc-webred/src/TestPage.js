391c9dc4e1fea43ffae5a53ebd523330aa8b67fa
Noah Kostesku <144395446+noahkostesku@users.noreply.github.com>
2025-02-23 21:30:41 -0500

    Update TestPage.js

--- fhc-webred/src/TestPage.js
+++ fhc-webred/src/TestPage.js
@@ -1,2 +1,16 @@
 import React from "react";
 import colours from "../assets/styles/BrandColours";
+
+//chnage with your logic import React from "react";
+// import MyComponent from "./components/MyComponent";
+
+// const TestPage = () => {
+//   return (
+//     <div>
+//       <h1>Testing Components</h1>
+//       <MyComponent />
+//     </div>
+//   );
+// };
+
+// export default TestPage;
