0ea1d1f3beb7193ead088b6b3cb29e73eaef7795
pmultani2 <multaniprabnoor71@gmail.com>
2025-03-05 20:14:43 -0500

    Created beneficiary card and progress bar

--- fhc-webred/src/components/DonationProgressBar.jsx
+++ fhc-webred/src/components/DonationProgressBar.jsx
@@ -1,2 +1,12 @@
 import React from "react";
 import colours from "../assets/styles/BrandColours";
+
+const ProgressBar = ({fraction}) => {
+  return (
+    <div className="w-full bg-[#D9D9D9] h-5">
+      <div className={`bg-[${colours.BLUE_YONDER}] h-5 w-${fraction}`}></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
