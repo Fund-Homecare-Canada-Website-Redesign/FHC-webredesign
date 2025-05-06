e8056fd258587958087cddb80ed9d8f58a46836e
Zekai Zhao <zzhao775@uwo.ca>
2025-05-06 11:46:03 -0400

    Add placeholder content for Admin Page

--- fhc-webred/src/pages/Admin.jsx
+++ fhc-webred/src/pages/Admin.jsx
@@ -1,2 +1,13 @@
 import React from "react";
 import colours from "../assets/styles/BrandColours";
+
+const Admin = () => {
+    return (
+        <div>
+            <h1>Admin</h1>
+            <p>This page is under construction</p>
+        </div>
+    );
+};
+
+export default Admin;
