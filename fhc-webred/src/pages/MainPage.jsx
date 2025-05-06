121b035bbb4d830a2847b4b69141acc91ddbcb85
Zekai Zhao <zzhao775@uwo.ca>
2025-05-06 11:46:38 -0400

    Add placeholder content for MainPage Page

--- fhc-webred/src/pages/MainPage.jsx
+++ fhc-webred/src/pages/MainPage.jsx
@@ -1,3 +1,13 @@
 import React from "react";
 import colours from "../assets/styles/BrandColours";
 
+const MainPage = () => {
+    return (
+        <div>
+            <h1>Main Page</h1>
+            <p>This page is under construction</p>
+        </div>
+    );
+};
+
+export default MainPage;
