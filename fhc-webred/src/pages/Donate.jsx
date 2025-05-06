98c654d2c17eac91514e1d716bfb3f78b548b3cd
Zekai Zhao <zzhao775@uwo.ca>
2025-05-06 11:46:29 -0400

    Add placeholder content for Donate Page

--- fhc-webred/src/pages/Donate.jsx
+++ fhc-webred/src/pages/Donate.jsx
@@ -1,2 +1,13 @@
 import React from "react";
 import colours from "../assets/styles/BrandColours";
+
+const Donate = () => {
+    return (
+        <div>
+            <h1>Donate</h1>
+            <p>This page is under construction</p>
+        </div>
+    );
+};
+
+export default Donate;
