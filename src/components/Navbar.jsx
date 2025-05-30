addf8a64b1a84fd7d987057a9d76216b9e33c66b
Zekai Zhao <zzhao775@uwo.ca>
2025-05-30 13:11:07 -0400

    Remove the background of logo image in nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,43 +11,43 @@ function NavbarComponent() {
 
   return (
     <>
-    {/* custom css for hover bars to be teal */}
-    <style>
-      {`
+      {/* custom css for hover bars to be teal */}
+      <style>
+        {`
         .nav-link:hover::before {
         background-color: #30AFAA; /* Custom teal color */
         }
       `}
-    </style>
-    <Navbar bg="light" expand="lg" className="shadow-sm py-2">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          <img
-            src={require('../assets/images/Logos/FHC_Title_Logo.jpg')}
-            alt="Fund Homecare Canada"
-            style={{ height: '40px' }}
-          />
-        </Navbar.Brand>
+      </style>
+      <Navbar bg="light" expand="lg" className="shadow-sm py-2">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img
+              src={require('../assets/images/Logos/FHC_Title_Logo_NoBgrd.png')}
+              alt="Fund Homecare Canada"
+              style={{ height: '40px' }}
+            />
+          </Navbar.Brand>
           {/* nav is react-bootstrap component for a section containing navigation links */}
           <Nav className="ml-auto"> {/* me = margin end, auto pushes all items following this component to the right, creates visual seperation */}
-            <Nav.Link as={Link} to="/" 
-            style={{fontFamily: 'Montserrat'}} 
-            className="nav-link font-semibold text-black hover:text-blue-600 mr-5 relative pb-1 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-blue-600 before:transition-all before:duration-300 hover:before:w-full">
-            Home
+            <Nav.Link as={Link} to="/"
+              style={{ fontFamily: 'Montserrat' }}
+              className="nav-link font-semibold text-black hover:text-blue-600 mr-5 relative pb-1 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-blue-600 before:transition-all before:duration-300 hover:before:w-full">
+              Home
             </Nav.Link>
-            
+
             <Nav.Link as={Link} to="/about-us" style={{ fontFamily: 'Montserrat' }} className="nav-link font-semibold text-black hover:text-blue-600 mr-5 relative pb-1 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-blue-600 before:transition-all before:duration-300 hover:before:w-full">About Us</Nav.Link>
-            <Nav.Link as={Link} to="/events"  style={{ fontFamily: 'Montserrat' }}className="nav-link font-semibold text-black hover:text-blue-600 mr-5 relative pb-1 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-blue-600 before:transition-all before:duration-300 hover:before:w-full">Events</Nav.Link>
+            <Nav.Link as={Link} to="/events" style={{ fontFamily: 'Montserrat' }} className="nav-link font-semibold text-black hover:text-blue-600 mr-5 relative pb-1 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-blue-600 before:transition-all before:duration-300 hover:before:w-full">Events</Nav.Link>
             <Nav.Link as={Link} to="/beneficiaries" style={{ fontFamily: 'Montserrat' }} className="nav-link font-semibold text-black hover:text-blue-600 mr-5 relative pb-1 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-blue-600 before:transition-all before:duration-300 hover:before:w-full">Beneficiaries</Nav.Link>
-            <Nav.Link as={Link} to="/contact-us"  style={{ fontFamily: 'Montserrat' }} className="nav-link font-semibold text-black hover:text-blue-600 mr-5 relative pb-1 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-blue-600 before:transition-all before:duration-300 hover:before:w-full">Contact Us</Nav.Link>
+            <Nav.Link as={Link} to="/contact-us" style={{ fontFamily: 'Montserrat' }} className="nav-link font-semibold text-black hover:text-blue-600 mr-5 relative pb-1 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-blue-600 before:transition-all before:duration-300 hover:before:w-full">Contact Us</Nav.Link>
           </Nav>
           <Link to="/donate">
             <Button style={{ fontFamily: 'Montserrat', backgroundColor: '#30AFAA', color: '#000000', border: '#30AFAA' }} className="font-bold py-2 px-2.5 rounded-md ml-3">
               Donate to Us
             </Button>
           </Link>
-      </Container>
-    </Navbar>
+        </Container>
+      </Navbar>
     </>
   );
 }
